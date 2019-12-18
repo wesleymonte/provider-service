528175a1691842de9217abf4f41c0bc2f4edc560
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-12-18 17:28:40 -0300

    Updated fogbow driver

--- drivers/fogbow/constants.py
+++ drivers/fogbow/constants.py
@@ -4,6 +4,7 @@ COMPUTE_REQUEST_MAX_TRIES_MESSAGE = "Failed: Maximum compute state verification 
 
 PUBLIC_IP_REQUEST_SUCCESSFUL_MESSAGE = "Successful: Attached a public IP to the VM."
 PUBLIC_IP_REQUEST_FAILED_MESSAGE = "Failed: Could not attach a public IP to the VM."
+PUBLIC_IP_REQUEST_ERROR_MESSAGE = "Error: Could not attach a public IP to the VM."
 PUBLIC_IP_REQUEST_MAX_TRIES_MESSAGE = "Failed: Maximum public ip state verification attempts reached."
 
 INTERVAL_CHECK_COMPUTE_STATE_SEC = 3
--- drivers/fogbow/fogbow.py
+++ drivers/fogbow/fogbow.py
@@ -18,12 +18,11 @@ def sync_get_compute(token, compute_id, interval, max_tries):
         compute_state = http_helper.get_compute(token, compute_id).get('state')
         tries += 1
     if(compute_state == ResourceState.READY.name):
-        return (True, constants.COMPUTE_REQUEST_SUCCESSFUL_MESSAGE)
+        logging.info(constants.COMPUTE_REQUEST_SUCCESSFUL_MESSAGE)
     elif(compute_state == ResourceState.FAILED.name):
-        return (False, constants.COMPUTE_REQUEST_FAILED_MESSAGE)
+        raise Exception(constants.COMPUTE_REQUEST_FAILED_MESSAGE))
     elif(tries == max_tries):
-        return (False, constants.COMPUTE_REQUEST_MAX_TRIES_MESSAGE)
-    return (False, "Runtime Error")
+        raise Exception(constants.COMPUTE_REQUEST_MAX_TRIES_MESSAGE))
 
 def sync_get_public_ip(token, public_ip_id, interval, max_tries):
     tries = 0
@@ -33,42 +32,38 @@ def sync_get_public_ip(token, public_ip_id, interval, max_tries):
         public_ip_state = http_helper.get_public_ip(token, public_ip_id).get('state')
         tries += 1
     if(public_ip_state == ResourceState.READY.name):
-        return (True, constants.PUBLIC_IP_REQUEST_SUCCESSFUL_MESSAGE)
+        logging.info(constants.PUBLIC_IP_REQUEST_SUCCESSFUL_MESSAGE)
     elif(public_ip_state == ResourceState.FAILED.name):
-        return (False, constants.PUBLIC_IP_REQUEST_FAILED_MESSAGE)
+        raise Exception(constants.PUBLIC_IP_REQUEST_FAILED_MESSAGE))
     elif (public_ip_state == ResourceState.ERROR.name):
-        return (False, constants.PUBLIC_IP_REQUEST_FAILED_MESSAGE)
+        raise Exception(constants.PUBLIC_IP_REQUEST_ERROR_MESSAGE)
     elif(tries == max_tries):
-        return (False, constants.PUBLIC_IP_REQUEST_MAX_TRIES_MESSAGE)
+        raise Exception(constants.PUBLIC_IP_REQUEST_MAX_TRIES_MESSAGE)
 
 def create_resource(token, compute_spec):
     compute_id = http_helper.create_compute(token, compute_spec)
-    result, message = sync_get_compute(token, compute_id, constants.INTERVAL_CHECK_COMPUTE_STATE_SEC, constants.MAX_TRIES)
-    logging.info(message)
-    if result:
+    try:
+        sync_get_compute(token, compute_id, constants.INTERVAL_CHECK_COMPUTE_STATE_SEC, constants.MAX_TRIES)
         public_ip_id = http_helper.create_public_ip(token, compute_id)
-        result, message = sync_get_public_ip(token, public_ip_id, constants.INTERVAL_CHECK_PUBLIC_IP_STATE_SEC, constants.MAX_TRIES)
-        logging.info(message)
-        if result:
-            resource = {'compute_id':compute_id, 'public_ip_id':public_ip_id}
-            return resource, "Successful resource create"
-        else:
-            http_helper.delete_public_ip(token, public_ip_id)
-            time.sleep(1)
-            http_helper.delete_compute(token, compute_id)
-            return None, message
-    else:
+        sync_get_public_ip(token, public_ip_id, constants.INTERVAL_CHECK_PUBLIC_IP_STATE_SEC, constants.MAX_TRIES)
+        return public_ip_id
+    except Exception as e:
+        http_helper.delete_public_ip(token, public_ip_id)
+        time.sleep(1)
         http_helper.delete_compute(token, compute_id)
-        return None, message
+        raise e
 
 def request_node(spec):
-    computeSpec = http_helper.ComputeSpec.from_json(spec)
-    my_token = http_helper.create_token()
-    resource, message = create_resource(my_token, computeSpec)
-    if resource is None:
-        logging.error("Error while requesting fogbow resource: " + message)
-        return None
-    else:
+    try:
+        computeSpec = http_helper.ComputeSpec.from_json(spec)
+        my_token = http_helper.create_token()
+        public_ip_id = create_resource(my_token, computeSpec)
         ip = http_helper.get_public_ip(my_token, resource.get('public_ip_id')).get('ip')
-        print("Got node from fogbow: " + ip)
         return ip
+    except Exception as e:
+        logging.error(str(e))
+
+def provider(node):
+    ip = request_node(node.get("spec"))
+    node["ip"] = ip
+    return ip
