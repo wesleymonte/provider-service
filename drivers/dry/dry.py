4e331a477e397f66fb3d8fa27fa5362ab65739e0
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-12-18 15:50:09 -0300

    Create dry driver

---
+++ drivers/dry/dry.py
@@ -0,0 +1,9 @@
+def check_spec(node):
+    spec = node.get("spec")
+    if spec == None or spec.get("ip") == None:
+        raise Exception("Invalid node specification [{}]".format(node.get("id")))
+
+def provider(node):
+    check_spec(node)
+    ip = node.get("spec").get("ip")
+    node["ip"] = ip
