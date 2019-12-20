cd405d423a1f6fb10bc90ef7adeeceebaae3c8a6
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-12-20 12:23:17 -0300

    Added to do to dry driver

--- drivers/dry/dry.py
+++ drivers/dry/dry.py
@@ -4,6 +4,7 @@ def check_spec(node):
         raise Exception("Invalid node specification [{}]".format(node.get("id")))
 
 def provider(node):
+    #TODO ssh ping
     check_spec(node)
     ip = node.get("spec").get("ip")
     node["ip"] = ip
